91d8bd70fe322f8542a86287007dece58ec82ca6
csteinmetz1 <csteinmetz1@gmail.com>
2023-11-01 12:21:43 +0000

    functional va modeling

--- examples/virtual_analog.py
+++ examples/virtual_analog.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import auraloss
 import torchaudio
@@ -9,6 +10,76 @@ from dasp_pytorch import ParametricEQ
 
 # This is a good example of how to chain processors together to create a more complex effect.
 # In this case, we create our own torch.nn.Module using the processor functions.
+
+
+def plot_waveforms(
+    src: torch.Tensor,
+    target: torch.Tensor,
+    pred: torch.Tensor,
+    num_segments: int = 6,
+    segment_length: int = 4096,
+):
+    fig, axs = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(8, 6))
+    axs = np.reshape(axs, -1)
+
+    for n in range(num_segments):
+        start_idx = np.random.randint(0, src.shape[-1] - segment_length)
+        stop_idx = start_idx + segment_length
+        # axs[n].plot(
+        #    src[0, start_idx:stop_idx].numpy(),
+        #    label="src",
+        #    alpha=0.7,
+        # )
+        axs[n].plot(
+            target[0, start_idx:stop_idx].numpy(),
+            label="target",
+            alpha=0.7,
+        )
+        axs[n].plot(
+            pred[0, start_idx:stop_idx].numpy(),
+            label="pred",
+            alpha=0.7,
+        )
+        axs[n].grid(c="lightgray")
+        if n == 0:
+            axs[n].legend()
+
+    plt.savefig("outputs/virtual_analog/audio.png", dpi=300)
+    plt.close("all")
+
+
+def plot_system(model: torch.nn.Module):
+    # plot response of pre and post filters
+    fig, axs = plt.subplots(1, 3, sharex=False, sharey=False)
+
+    # plot pre-filter
+    axs[0].set_aspect("equal", "box")
+
+    # plot nonlinearity
+    x = torch.linspace(-3, 3, 1000)
+    with torch.no_grad():
+        y = model.mlp_nonlinearity(x.unsqueeze(1)).squeeze(1)
+
+    axs[1].plot(x, y)
+    axs[1].grid(c="lightgray")
+    axs[1].set_title("Nonlinearity")
+    axs[1].set_xlim(-3, 3)
+    axs[1].set_ylim(-3, 3)
+    axs[1].set_aspect("equal", "box")
+
+    # plot post-filter
+    axs[2].set_aspect("equal", "box")
+
+    plt.savefig("outputs/virtual_analog/system.png", dpi=300)
+
+
+def plot_loss(loss_history: list):
+    plt.figure()
+    plt.plot(loss_history)
+    plt.grid(c="lightgray")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.savefig("outputs/virtual_analog/loss.png", dpi=300)
 
 
 class FileDataset(torch.utils.data.Dataset):
@@ -52,17 +123,18 @@ class DistortionModel(torch.nn.Module):
         # so we can use the same equalizer for the pre and post filters
         self.equalizer = ParametricEQ(sample_rate)
 
+        self.input_gain = torch.nn.Parameter(torch.tensor(0.0))
         # create parameters for the pre and post filters (these will be optimized)
         self.pre_filter_params = torch.nn.Parameter(
             torch.rand(1, self.equalizer.num_params)
         )
         self.dc_offset = torch.nn.Parameter(torch.tensor(0.0))
         self.mlp_nonlinearity = torch.nn.Sequential(
-            torch.nn.Linear(1, 64),
-            torch.nn.ReLU(),
-            torch.nn.Linear(64, 64),
-            torch.nn.ReLU(),
-            torch.nn.Linear(64, 1),
+            torch.nn.Linear(1, 128),
+            torch.nn.PReLU(),
+            torch.nn.Linear(128, 128),
+            torch.nn.PReLU(),
+            torch.nn.Linear(128, 1),
         )
         self.post_filter_params = torch.nn.Parameter(
             torch.rand(1, self.equalizer.num_params)
@@ -77,13 +149,16 @@ class DistortionModel(torch.nn.Module):
         """
         batch_size, n_channels, n_samples = x.shape
 
+        # apply input gain
+        x = x * torch.sigmoid(self.input_gain) * 24.0
+
         # apply pre-filter
         y = self.equalizer.process_normalized(
             x,
             torch.sigmoid(self.pre_filter_params),
         )
         # apply dc offset
-        y = y + self.dc_offset
+        # y = y + self.dc_offset
         # y = torch.tanh(y)  # apply non-linearity
         y = self.mlp_nonlinearity(y.permute(0, 2, 1)).permute(0, 2, 1)
         # apply post-filter
@@ -95,57 +170,27 @@ class DistortionModel(torch.nn.Module):
         return y
 
 
-def plot_waveforms(
-    src: torch.Tensor,
-    target: torch.Tensor,
-    pred: torch.Tensor,
-    num_segments: int = 6,
-    segment_length: int = 4096,
-):
-    fig, axs = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(8, 6))
-    axs = np.reshape(axs, -1)
-
-    for n in range(num_segments):
-        start_idx = n * segment_length
-        stop_idx = start_idx + segment_length
-        # axs[n].plot(
-        #    src[0, start_idx:stop_idx].numpy(),
-        #    label="src",
-        #    alpha=0.7,
-        # )
-        axs[n].plot(
-            target[0, start_idx:stop_idx].numpy(),
-            label="target",
-            alpha=0.7,
-        )
-        axs[n].plot(
-            pred[0, start_idx:stop_idx].numpy(),
-            label="pred",
-            alpha=0.7,
-        )
-        axs[n].grid(c="lightgray")
-        if n == 0:
-            axs[n].legend()
-
-    plt.savefig("virtual_analog.png", dpi=300)
-    plt.close("all")
-
-
-def plot_system(model: torch.nn.Module):
-    # plot response of pre and post filters
-    fig, axs = plt.subplots(1, 3, sharex=True, sharey=True)
-
-
 def train(
     model: torch.nn.Module,
     dataloader: torch.utils.data.DataLoader,
-    lr: float = 1e-2,
-    epochs: int = 500,
+    sample_rate: int,
+    lr: float = 1e-3,
+    epochs: int = 1000,
     use_gpu: bool = False,
 ):
     # loss function in frequency domain
-    freq_criterion = auraloss.freq.STFTLoss()
-    time_critertion = torch.nn.MSELoss()
+    fd_loss_fn = auraloss.freq.MultiResolutionSTFTLoss(
+        fft_sizes=[128, 256, 512, 1024, 2048, 4096, 8192],
+        hop_sizes=[64, 128, 256, 512, 1024, 2048, 4096],
+        win_lengths=[128, 256, 512, 1024, 2048, 4096, 8192],
+        w_sc=0.0,
+        w_phs=0.0,
+        w_lin_mag=1.0,
+        w_log_mag=1.0,
+        perceptual_weighting=True,
+        sample_rate=sample_rate,
+    )
+    td_loss_fn = torch.nn.MSELoss()
 
     # create optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -153,10 +198,12 @@ def train(
 
     if use_gpu:
         model.cuda()
-        freq_criterion.cuda()
+        fd_loss_fn.cuda()
 
+    epoch_loss_history = []
     for epoch in range(epochs):
         pbar = tqdm(dataloader)
+        loss_history = []
         for batch in pbar:
             src, target = batch
 
@@ -168,9 +215,9 @@ def train(
             y_hat = model(src)
 
             # compute loss between estimate and target
-            freq_loss = freq_criterion(y_hat, target)
-            time_loss = time_critertion(y_hat, target)
-            loss = freq_loss + (100 * time_loss)
+            freq_loss = fd_loss_fn(y_hat, target)
+            time_loss = td_loss_fn(y_hat, target)
+            loss = freq_loss + (time_loss * 100.0)
 
             # optimize
             optimizer.zero_grad()
@@ -178,24 +225,32 @@ def train(
             optimizer.step()
             scheduler.step()
 
-            pbar.set_description(f"Epoch: {epoch+1}/{epochs}  Loss: {loss.item():.3f}")
+            loss_history.append(loss.item())
 
-        # plot the results
-        # plot_system(model)
+            pbar.set_description(
+                f"Epoch: {epoch+1}/{epochs}  Loss: {np.mean(loss_history):.3e}"
+            )
+        # average loss over epoch
+        epoch_loss_history.append(np.mean(loss_history))
+
+    return epoch_loss_history
 
 
 if __name__ == "__main__":
     # construct dataset
-    scr_filepath = "audio/va/clean.wav"
-    target_filepath = "audio/va/sparkle-combo-crunch.wav"
-    dataset = FileDataset(scr_filepath, target_filepath)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
+    scr_filepath = "audio/va/6amps/idmt-rock-input-varying-gain.wav"
+    target_filepath = "audio/va/6amps/idmt-rock-high-gain1-brit-8000.wav"
+    dataset = FileDataset(scr_filepath, target_filepath, length=32768)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+
+    # directory for outputs
+    os.makedirs("outputs/virtual_analog", exist_ok=True)
 
     # construct grey-box distortion model
     model = DistortionModel(sample_rate=44100)
 
     # train the model!
-    train(model, dataloader, use_gpu=True)
+    loss_history = train(model, dataloader, sample_rate=44100, use_gpu=True)
 
     # run the final model and plot the results
     model.cpu()
@@ -210,10 +265,13 @@ if __name__ == "__main__":
         y_hat = model(src.unsqueeze(0)).squeeze(0)
 
     plot_waveforms(src, target, y_hat)
+    plot_system(model)
+    plot_loss(loss_history)
 
     # save audio
+    filename = os.path.basename(target_filepath).replace(".wav", "")
     torchaudio.save(
-        "audio/va/sparkle-combo-crunch-pred.wav",
+        f"audio/va/6amps/{filename}-pred.wav",
         y_hat,
         sr,
         backend="soundfile",
